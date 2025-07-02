be1d540807fb86eae4006958b00998f731af43c9
park0609 <guppy135@naver.com>
2025-07-02 09:27:03 +0900

    07.02 commit
    파일 수정

---
+++ Day_08/backboard/src/main/java/com/pknu/backboard/repository/ReplyRepository.java
@@ -0,0 +1,12 @@
+package com.pknu.backboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pknu.backboard.entity.Reply;
+
+@Repository
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    // 내용은 필요없음. JpaRepository에 기능이 존재
+
+}
